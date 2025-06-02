b01745f887d6529561c2f54a7d2037c909263c7e
rumapro <rumapro6@gmail.com>
2025-06-02 14:53:10 +0900

    재배포포

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,8 +6,8 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} /> {/* 전체조회 */}
-        <Route path="/:playerId" element={<App />} /> {/* 특정 playerId 조회 */}
+        <Route path="/:playerId" element={<App />} /> {/* 특정 playerId 조회 */}{/* 에러 해결용 */}
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> 
   );
 }
